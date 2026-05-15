67146da5b34b4cee730a3d8af83a56e5fafe6d90
pablo <pablo@quiroz.me>
2026-05-15 19:03:51 -0400

    Add project files.

---
+++ Algoritmos de Planificación/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Algoritmos_de_Planificación
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
