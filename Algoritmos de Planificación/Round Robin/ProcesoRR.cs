2c7912f801834c89b0b7124e9f65c51307ac24e4
pablo <pablo@quiroz.me>
2026-05-19 02:01:08 -0400

    feat: add Round Robin completed

---
+++ Algoritmos de Planificación/Round Robin/ProcesoRR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmos_de_Planificación.Round_Robin
+{
+    public class ProcesoRR
+    {
+        public string IdProceso { get; set; } = "";
+        public string Cliente { get; set; } = "";
+        public string Problema { get; set; } = "";
+
+        // Tiempo total que requiere el proceso para terminar
+        public int BurstTime { get; set; }
+
+        // Guarda el progreso del proceso cuando es interrumpido
+        public int TiempoRestante { get; set; }
+
+        public int WaitingTime { get; set; }
+        public int TurnaroundTime { get; set; }
+
+        // Manejo de estados (Listo, En Ejecución, Bloqueado, Finalizado) como pide la rúbrica
+        public string Estado { get; set; } = "Listo";
+    }
+}
